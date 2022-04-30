c0f292012e4b83a54fb400d0e5544ea7cbb239e3
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-30 14:00:40 -0700

    commit tree c81b6f757e57e35fd1ceabc082c0a573a8e6f702

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :accepted
+
+  end
+
+  def show
+    items = User.find(params[:user_id]).items
+    item = items.find(params[:id])
+    render json: item, status: :accepted
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_error
+    render json: {error: "That user_id doesn't exist."}, status: :not_found
   end
 
 end
